8d3b7ddb5b30ed5dfd57c5cabf201a492332a8a1
Andreas Kugel <andreas.kugel@web.de>
2023-05-02 19:56:27 +0200

    update simple tf network example. dense only + cnn

--- info/ml/tfImage/src/main.ts
+++ info/ml/tfImage/src/main.ts
@@ -28,7 +28,7 @@ const imgClasses = 3;
 const imgSize = 64;
 const numImgs = 256;
 
-const labelNames = ["Tringle","Ellipse","Rectangle"]
+const labelNames = ["Tringle", "Ellipse", "Rectangle"]
 
 //await imgGen();
 
@@ -42,30 +42,30 @@ async function tfTest() {
 
   let errors = 0
   let numTests = 100
-  for (let i = 0; i<numTests;i++){
+  for (let i = 0; i < numTests; i++) {
     const evalImg = await mkSingleImg();
     console.log("Eavl img rdy");
-  
+
     // Convert the input image to a TensorFlow tensor
     const inputTensor = (await evalImg).image.reshape([1, imgSize, imgSize, 1]);
     // Run the model on the input tensor
     const outputTensor = await model.predict(inputTensor);
-  
+
     // Convert the output tensor to a JavaScript array
     const outputArray = await outputTensor.dataSync();
-  
+
     // Get the predicted class index
     const predictedIndex = outputArray.indexOf(Math.max(...outputArray));
     if (predictedIndex != evalImg.label) {
       console.log("Error")
       errors++
     }
-    console.log("Expected:",evalImg.label)
+    console.log("Expected:", evalImg.label)
     console.log(`The predicted class index is ${predictedIndex} - ${labelNames[predictedIndex]}.`);
     await new Promise((resolve) => setTimeout(resolve, 100));
   }
-  console.log("Totoal errors:",errors," -> ", errors/numTests * 100 ,"%")
-  alert("Error rate: " + String(errors/numTests * 100)  + "%")
+  console.log("Totoal errors:", errors, " -> ", errors / numTests * 100, "%")
+  alert("Error rate: " + String(errors / numTests * 100) + "%")
 }
 
 async function setupModel() {
@@ -127,15 +127,55 @@ async function setupModel() {
   //console.log("Trainy:",trainYs)
   console.log("Train/test prepared");
 
+  let useCnn = true
+  let model
+  let epochs
   // Train and evaluate a simple neural network using the training and test data
-  const model = tf.sequential({
-    layers: [
-      tf.layers.flatten({ inputShape: [imgSize, imgSize, 1] }),
-      tf.layers.dense({ units: imgSize, activation: "relu" }),
-      tf.layers.dense({ units: imgClasses, activation: "softmax" }),
-    ],
-  });
+  if (useCnn) {
+    epochs = 100
+    model = tf.sequential();
 
+    // Convolutional layers
+    model.add(tf.layers.conv2d({
+      inputShape: [imgSize, imgSize, 1],
+      filters: 16,
+      kernelSize: 3,
+      activation: 'relu'
+    }));
+    model.add(tf.layers.maxPooling2d({ poolSize: 2 }));
+    model.add(tf.layers.conv2d({
+      filters: 32,
+      kernelSize: 5, // 3
+      activation: 'relu'
+    }));
+    model.add(tf.layers.maxPooling2d({ poolSize: 3 })); // 2
+
+    // Flatten layer to prepare for fully-connected layers
+    model.add(tf.layers.flatten());
+
+    // Fully-connected layers
+    model.add(tf.layers.dense({ units: imgSize, activation: 'relu' }));
+    model.add(tf.layers.dense({ units: imgClasses, activation: 'sigmoid' }));
+
+    // Compile the model
+    model.compile({
+      optimizer: 'adam',
+      loss: 'binaryCrossentropy',
+      metrics: ['accuracy']
+    });
+
+  } else {
+    epochs = 200
+    model = tf.sequential({
+      layers: [
+        tf.layers.flatten({ inputShape: [imgSize, imgSize, 1] }),
+        tf.layers.dense({ units: imgSize, activation: "relu" }),
+        tf.layers.dense({ units: imgClasses, activation: "softmax" }),
+      ],
+    });
+
+
+  }
   model.compile({
     optimizer: "adam",
     loss: "categoricalCrossentropy",
@@ -143,8 +183,7 @@ async function setupModel() {
   });
 
 
-  const batchSize = numImgs/4;
-  const epochs = 200
+  const batchSize = numImgs / 4;
 
   // Define a callback function to monitor the progress of the training
   const monitorCallback = async (epoch, logs) => {
@@ -199,81 +238,81 @@ async function setupModel() {
 // --------------------------------------------------
 // --------------------------------------------------
 async function showWeights(weights) {
-// Assume that you have a trained model called "model" and a dense layer called "denseLayer"
+  // Assume that you have a trained model called "model" and a dense layer called "denseLayer"
 
-// Get the weights tensor of the dense layer
-const weightsArray = await weights[0].array();
-const numRows = weights[0].shape[0];
-const numCols = weights[0].shape[1];
+  // Get the weights tensor of the dense layer
+  const weightsArray = await weights[0].array();
+  const numRows = weights[0].shape[0];
+  const numCols = weights[0].shape[1];
 
-// Find the minimum and maximum values in the weights array
-const minValue = Math.min(...weightsArray.flat());
-const maxValue = Math.max(...weightsArray.flat());
+  // Find the minimum and maximum values in the weights array
+  const minValue = Math.min(...weightsArray.flat());
+  const maxValue = Math.max(...weightsArray.flat());
 
-const numElements = numRows * numCols
-const squareSize = Math.ceil(Math.sqrt(numElements));
-const reshapedArray = new Array(squareSize).fill().map(() => new Array(squareSize).fill(0));
-for (let i = 0; i < numElements; i++) {
-  const row = Math.floor(i / squareSize);
-  const col = i % squareSize;
-  reshapedArray[row][col] = weightsArray[Math.floor(i / numCols)][i % numCols];
-}
-//console.log("reshaped:",reshapedArray)
-
-// Create a new canvas element to display the weights
-const canvas = document.getElementById('cvw');
-/*
-canvas.width = numCols;
-canvas.height = numRows;
-*/
-
-canvas.width = squareSize;
-canvas.height = squareSize;
-
-// will rescale to this size
-/*
-canvas.width = imgSize
-canvas.height = imgSize;
-*/
-// Draw the weights array on the canvas as a grayscale image
-const context = await canvas.getContext('2d');
-const imageData = await context.createImageData(squareSize,squareSize);
-const data = imageData.data;
-/*
-for (let i = 0; i < numRows; i++) {
-  for (let j = 0; j < numCols; j++) {
-    const index = (i * numCols + j) * 4;
-    const value = weightsArray[i][j];
-    data[index] = value * 255;
-    data[index + 1] = value * 255;
-    data[index + 2] = value * 255;
-    data[index + 3] = 255;
+  const numElements = numRows * numCols
+  const squareSize = Math.ceil(Math.sqrt(numElements));
+  const reshapedArray = new Array(squareSize).fill().map(() => new Array(squareSize).fill(0));
+  for (let i = 0; i < numElements; i++) {
+    const row = Math.floor(i / squareSize);
+    const col = i % squareSize;
+    reshapedArray[row][col] = weightsArray[Math.floor(i / numCols)][i % numCols];
   }
-}
-*/
-for (let i = 0; i < squareSize; i++) {
-  for (let j = 0; j < squareSize; j++) {
-    const index = (i * squareSize + j) * 4;
-    const value = reshapedArray[i][j];
+  //console.log("reshaped:",reshapedArray)
 
-    const normalizedValue = (value - minValue) / (maxValue - minValue);
-    const red = Math.round(normalizedValue * 255);
-    const blue = Math.round((1 - normalizedValue) * 255);
-    //console.log(red,blue)
+  // Create a new canvas element to display the weights
+  const canvas = document.getElementById('cvw');
+  /*
+  canvas.width = numCols;
+  canvas.height = numRows;
+  */
 
-    data[index] = red;
-    data[index + 1] = 0;
-    data[index + 2] = blue;
-    /*
-    data[index] = value * 255;
-    data[index + 1] = value * 255;
-    data[index + 2] = value * 255;
-    */
-    data[index + 3] = 255;
+  canvas.width = squareSize;
+  canvas.height = squareSize;
+
+  // will rescale to this size
+  /*
+  canvas.width = imgSize
+  canvas.height = imgSize;
+  */
+  // Draw the weights array on the canvas as a grayscale image
+  const context = await canvas.getContext('2d');
+  const imageData = await context.createImageData(squareSize, squareSize);
+  const data = imageData.data;
+  /*
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      const index = (i * numCols + j) * 4;
+      const value = weightsArray[i][j];
+      data[index] = value * 255;
+      data[index + 1] = value * 255;
+      data[index + 2] = value * 255;
+      data[index + 3] = 255;
+    }
   }
-}
-//context.putImageData(imageData, 0, 0);
-await context.putImageData(imageData, 0, 0);
+  */
+  for (let i = 0; i < squareSize; i++) {
+    for (let j = 0; j < squareSize; j++) {
+      const index = (i * squareSize + j) * 4;
+      const value = reshapedArray[i][j];
+
+      const normalizedValue = (value - minValue) / (maxValue - minValue);
+      const red = Math.round(normalizedValue * 255);
+      const blue = Math.round((1 - normalizedValue) * 255);
+      //console.log(red,blue)
+
+      data[index] = red;
+      data[index + 1] = 0;
+      data[index + 2] = blue;
+      /*
+      data[index] = value * 255;
+      data[index + 1] = value * 255;
+      data[index + 2] = value * 255;
+      */
+      data[index + 3] = 255;
+    }
+  }
+  //context.putImageData(imageData, 0, 0);
+  await context.putImageData(imageData, 0, 0);
 
 
 
@@ -337,7 +376,7 @@ async function mkSingleImg() {
   // Define shape properties
   const shapeSize = Math.floor((Math.random() * imgSize) / 4) + imgSize / 6; // random size between 10 and 30
   const shapeStrokeWidth = Math.floor(Math.random() * 4) + 3; // random stroke width between 1 and 4
-  const shapeRotation = Math.random() * 2*Math.PI; // random rotation between 0 and pi
+  const shapeRotation = Math.random() * 2 * Math.PI; // random rotation between 0 and pi
 
   // Set shape position to center of canvas
   /* */
@@ -345,9 +384,9 @@ async function mkSingleImg() {
   //const shapeY = canvas.height / 2;
   /* */
   //
-    const shapeX = (imgSize / 2) + Math.floor((Math.random() -.5) * shapeSize/4)
-    const shapeY =  (imgSize / 2) + Math.floor((Math.random() -.5) * shapeSize/4)
-    //
+  const shapeX = (imgSize / 2) + Math.floor((Math.random() - .5) * shapeSize / 4)
+  const shapeY = (imgSize / 2) + Math.floor((Math.random() - .5) * shapeSize / 4)
+  //
 
   // Draw shape based on type and add label
   let label;
@@ -383,7 +422,7 @@ async function mkSingleImg() {
   // NOTE: we have black+white data. color channels are all 0, alpha channel is 0 or 255
   const imageDataArray = await new Float32Array(imageData.data.length / 4);
   for (let i = 0; i < imageData.data.length; i += 4) {
-    const gray = imageData.data[i+3]
+    const gray = imageData.data[i + 3]
     const gv = gray / 255
     imageDataArray[i / 4] = gv
   }
