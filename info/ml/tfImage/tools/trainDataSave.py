b924025ca4bf5062c41c929004096b3971156519
Andreas Kugel <andreas.kugel@web.de>
2023-11-03 12:30:44 +0100

    extract ftain images

---
+++ info/ml/tfImage/tools/trainDataSave.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from PIL import Image, ImageDraw
+import os
+
+df = pd.read_json("/home/kugel/Downloads/datencafe-traindata2.json")
+shape = df.tensor.iloc[0]["shape"]
+
+outPath = "trainImg"
+try:
+    os.mkdir(outPath)
+except:
+    pass
+
+
+def extract(idx):
+    data = df.iloc[idx]["imgdata"]
+    lbl = df.iloc[idx]["names"]
+    lst = [data[x] for x in data]
+    if shape[2] > 1:
+        ar = np.reshape(lst,shape)
+    else:
+        ar = np.reshape(lst,shape[:2])
+
+    img = Image.fromarray(ar*255)
+    img_inv = Image.fromarray((1-ar)*255)
+
+    fn = os.sep.join([outPath,f"{lbl}_{idx:04}.png"])
+    # print(fn)
+    # img.show()
+    # can only save RGB as PNG
+    img.convert("RGB").save(fn)
+
+
+for idx in list(df.index):
+    extract(idx)
+
+#img0.show()
